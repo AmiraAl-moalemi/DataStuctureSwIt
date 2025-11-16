497435ce1e608ff3d544320b6594d91e82ce3887
Amira Al-moalemi <amiranomanalmoalemi@gmail.com>
2025-11-16 19:31:43 +0300

    lap1

---
+++ src/Lap1.java
@@ -0,0 +1,14 @@
+public class Lap1 {
+    public class Main {
+        public void main(String[] args){
+            System.out.println("hallo");
+        }
+    }
+}
+
+
+
+
+
+
+//https://github.com/AmiraAl-moalemi/DataStuctureSwIt.git
